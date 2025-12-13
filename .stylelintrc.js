28023eeb40ba6cde7514ae00f4b4ce46ca95e955
Takayuki Kumagai <xtakayukikumagai@gmail.com>
2025-12-13 17:07:17 +0900

    update stylelint setting

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
-  "extends": "stylelint-config-standard-scss", // SCSS対応の標準ルールを継承
-  "customSyntax": "postcss-scss",             // SCSS構文の解析に必要
-  "rules": {
+  extends: [
+    "stylelint-config-prettier-scss",
+    "stylelint-config-standard-scss", // SCSS対応の標準ルールを継承
+    "stylelint-config-recommended-scss",
+    "stylelint-config-clean-order",
+  ],
+  customSyntax: "postcss-scss", // SCSS構文の解析に必要
+  rules: {
     // SCSS特有のルールや、その他のカスタムルールをここに追加・上書き
     "scss/at-extend-no-missing-placeholder": true,
-    "indentation": 2,
-    "color-hex-case": "lower", // 16進数カラーコードを小文字に強制
-    "max-nesting-depth": 3 // ネストの深さを制限
-  }
-}
+    "max-nesting-depth": 3, // ネストの深さを制限
+  },
+};
